1a8c1d6773d1ae1809932d44025396f55819e78a
Morgan Thomas <u1707737@warwick.ac.uk>
2019-11-25 11:06:36 +0000

    GLUT inititalisation added.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
-#include "stdio.h"
+#include <GL/glew.h>
+#include <GL/glut.h>
 
-int main() {
-    if (!init()) {
+#include "stdio.h"
+#include "string.h"
+
+#define WIN_TITLE "Snake"
+#define WIN_WIDTH 512
+#define WIN_HEIGHT 512
+
+#define DEBUG true
+
+int init(int* argc, char** argv);
+
+int main(int argc, char* argv[]) {
+    if (!init(&argc, argv)) {
         fprintf(stderr, "ERROR: Failed to initialise.\n");
         return -1;
     }
@@ -9,8 +21,14 @@ int main() {
     return 0;
 }
 
-int init() {
-    
+int init(int* argc, char** argv) {
+    glutInit(argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
+    glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
+    glutCreateWindow("Snake");
+
+    if (DEBUG)
+        printf("Initialised.\n");
 
     return 1;
 }
