58df8fb503b35fa0d3834abdba430104e190bd5a
Morgan Thomas <morgan.111@live.co.uk>
2020-01-12 17:29:09 +0000

    Refactored code

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,9 @@
 
 using namespace std;
 
-Shader* colorShader;
-Shader* texShader;
+Shader* shader;
 
-Texture* sandTex;
+Texture* grassTex;
 Model* plane;
 Model* cube;
 
@@ -28,24 +27,18 @@ Model* cube;
 *    GLUT functions    *
 *----------------------*/
 
+const glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)WIN_WIDTH/WIN_HEIGHT, 0.1f, 100.0f);
+const glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+
 void gDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glBindVertexArray(0);
-
-	texShader->use();
-
-	// Set proj and view matricies
-	glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)WIN_WIDTH/WIN_HEIGHT, 0.1f, 100.0f);
-	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
-	texShader->setMat4f("proj", proj);
-	texShader->setMat4f("view", view);
-
 	// Set plane's model matrix
 	glm::mat4 model = glm::rotate(glm::mat4(1.0f), glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	texShader->setMat4f("model", model);
+	shader->setMat4f("model", model);
 
 	// Draw the plane
+	grassTex->bind();
 	plane->draw();
 
 	glutSwapBuffers();
@@ -61,10 +54,15 @@ void gLoop(int) {
 *    Init functions    *
 *----------------------*/
 
-// Initialise OpenGL things
-int init(int* argc, char** argv) {
+int main(int argc, char* argv[]) {
+	// For fixing stdout buffering on windows
+	#ifdef _WIN32
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	#endif
+
 	// Initialise GLUT window
-	glutInit(argc, argv);
+	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
 	glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
 	glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH) - WIN_WIDTH)/2, (glutGet(GLUT_SCREEN_HEIGHT) - WIN_HEIGHT)/2);
@@ -77,22 +75,27 @@ int init(int* argc, char** argv) {
 	GLenum err = glewInit();
 	if(err != GLEW_OK) {
 		fprintf(stderr, "ERROR: %s\n", glewGetErrorString(err));
-		return 0;
+		return -1;
 	}
 	printf("Initialised GLEW.\n");
 
-	// Create the shaders
-	colorShader = new Shader("shaders/color.vert", "shaders/color.frag");
-	texShader = new Shader("shaders/tex.vert", "shaders/tex.frag");
+	// Create the shader
+	shader = new Shader("shaders/shader.vert", "shaders/shader.frag");
+	shader->use();
 	printf("Compiled shaders.\n");
 
-	// Create the textures
-	sandTex = new Texture("tex/red_sand.png");
+	// Setup proj and view matricies and put them in to the shader
+	glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)WIN_WIDTH/WIN_HEIGHT, 0.1f, 100.0f);
+	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+	shader->setMat4f("proj", proj);
+	shader->setMat4f("view", proj);
 
-	// Create the models
-	plane = new PlaneModel(*sandTex);
-	cube = new CubeModel(*sandTex);
+	// Crete models and textures
+	grassTex = new Texture("img/grass.jpg");
+	plane = new PlaneModel(*grassTex);
+	cube = new CubeModel(*grassTex);
 
+	// Print OpenGL debug info
 	printf("Finished initialisation.\n\n");
 	printf("OpenGL Version: %s\nGLEW Version: %s\n\n", glGetString(GL_VERSION), glewGetString(GLEW_VERSION));
 
@@ -100,23 +103,6 @@ int init(int* argc, char** argv) {
 	glutDisplayFunc(gDisplay);
 	glutTimerFunc(1000/FPS, gLoop, 0);
 	glutMainLoop();
-
-	return 1;
-}
-
-// Entry function
-int main(int argc, char* argv[]) {
-	// For fixing stdout buffering on windows
-	#ifdef _WIN32
-	setvbuf(stdout, NULL, _IONBF, 0);
-	setvbuf(stderr, NULL, _IONBF, 0);
-	#endif
-
-	// Check if initialisation is successful
-	if (!init(&argc, argv)) {
-		fprintf(stderr, "ERROR: Failed to initialise.\n");
-		return -1;
-	}
 
 	return 0;
 }
